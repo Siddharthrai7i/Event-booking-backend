c3f6aefa66b195992a8e682634bb9d9c527463ac
Siddharth <siddharth23153065@akgec.ac.in>
2026-02-27 14:12:49 +0530

    Paymant Sevices Implementation

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-from app.api.v1 import auth, events
+from app.api.v1 import auth, events, booking ,  payment
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth.router)
 router.include_router(events.router)
+router.include_router(booking.router)
+router.include_router(payment.router)
 # coming soon:
 # router.include_router(booking.router)
 # router.include_router(payment.router)
